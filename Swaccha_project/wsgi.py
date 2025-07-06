4e988ec400bee46b07a2df5615f86512a3899c2b
SukharamD <sukharama@gmail.com>
2025-07-06 17:16:10 +0530

    Fix: corrected wsgi.py for static file serving

--- Swaccha_project/wsgi.py
+++ Swaccha_project/wsgi.py
@@ -1,20 +1,9 @@
-"""
-WSGI config for Swaccha_project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
-"""
-
 import os
 from whitenoise import WhiteNoise
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+from Swaccha_project.settings import STATIC_ROOT
 
-from Swaccha_project.settings import BASE_DIR
-BASE_DIR = Path(__file__).resolve().parent.parent
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Swaccha_project.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=BASE_DIR / 'staticfiles')
+application = WhiteNoise(application, root=STATIC_ROOT)
