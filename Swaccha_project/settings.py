aa4f137c34aab6e03812f37e2cdaebe93f9786db
SukharamD <sukharama@gmail.com>
2025-07-07 19:58:32 +0530

    Set Debug to False and changed allowed hosts

--- Swaccha_project/settings.py
+++ Swaccha_project/settings.py
@@ -21,14 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = 'True'
 
-# ALLOWED_HOSTS = ['.onrender.com', 'swacha.onrender.com', '127.0.0.1', 'localhost']
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
-
-
+ALLOWED_HOSTS = ['.onrender.com', 'swacha.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
