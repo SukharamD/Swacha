d312efd8b34ce1a866b16ffdd4d839a0960ac067
SukharamD <sukharama@gmail.com>
2025-07-07 16:05:25 +0530

    fixed static files

--- Swaccha_project/settings.py
+++ Swaccha_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myAuth',
     'profiles',
-    'booking',
+    'booking'
 ]
 
 MIDDLEWARE = [
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Swaccha_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'myAuth' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
