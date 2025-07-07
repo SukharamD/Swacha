c46313c2c28e00413af298a5f31fa91ddad4f7fc
SukharamD <sukharama@gmail.com>
2025-07-07 13:50:49 +0530

    Add requirements.txt for deployment

--- Swaccha_project/settings.py
+++ Swaccha_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', 'swacha.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'swacha.onrender.com', '127.0.0.1', 'localhost']
 
 
 
@@ -73,11 +73,13 @@ WSGI_APPLICATION = 'Swaccha_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
